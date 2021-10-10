6c41d9947945780eeb5b0edb2f64605b50f87087
Evengarr <Ravasect@gmail.ru>
2021-10-10 18:30:48 +0300

    Initial commit

---
+++ src/main/java/ru/geekbrains/patterns/ArchitecturesAndDesignPatternsApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.patterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchitecturesAndDesignPatternsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArchitecturesAndDesignPatternsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/geekbrains/patterns/ArchitecturesAndDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.patterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchitecturesAndDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
